fd76fada46b12aefbbd4baaaf52c675c4e4a4325
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-12 17:26:00 -0300

    Cria o projeto do Desafio de Projeto 02: Construindo um Sistema para um Estacionamento. Criação da classe Estacionamento.

---
+++ Projeto 02 - Sistema para Estacionamento/Estacionamento/Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,76 @@
+﻿using System.Numerics;
+
+namespace Estacionamento.Models
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial;
+        private decimal precoHora;
+        private List<string> veiculos = new();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora) 
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            string placa;
+
+            Console.WriteLine("Digite a placa do veículo: ");
+            placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Any(placaVeiculo => placaVeiculo == placa))
+            {
+                Console.WriteLine("Este veículo já se encontra estacionado.");
+            }
+            else
+            {
+                veiculos.Add(placa);
+            }
+
+            Console.WriteLine("Veículo adicionado com sucesso!");
+        }
+
+        public void RemoverVeiculo()
+        {
+            string placa;
+
+            Console.WriteLine("Digite a placa do veículo: ");
+            placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Any(placaVeiculo => placaVeiculo == placa))
+            {
+                int quantidadeHoras;
+                Console.WriteLine("Digite a quantidade de horas que o veículo ficou estacionado: ");
+                while (int.TryParse(Console.ReadLine(), out quantidadeHoras) || quantidadeHoras < 0)
+                {
+                    Console.WriteLine("Valor inválido");
+                }
+
+                veiculos.Remove(placa);
+                Console.WriteLine($"Veículo removido. Valor total do estacionamento: {precoInicial + precoHora * quantidadeHoras}");
+            }
+            else
+            {
+                Console.WriteLine("Não há nenhum veículo estacionado com esta placa");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                foreach(string placa in veiculos)
+                {
+                    Console.WriteLine(placa);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
