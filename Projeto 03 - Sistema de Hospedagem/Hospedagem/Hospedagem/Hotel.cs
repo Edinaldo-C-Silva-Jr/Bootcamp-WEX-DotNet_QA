23ba1c6c645285cb4153c938c2a97c9720a308ec
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-20 03:19:39 -0300

    Cria a classe Hotel para gerenciar os cadastros realizados pelo menu. Também melhora as validações de todas as entradas de dados.

---
+++ Projeto 03 - Sistema de Hospedagem/Hospedagem/Hospedagem/Hotel.cs
@@ -0,0 +1,258 @@
+﻿using Hospedagem.Models;
+
+namespace Hospedagem
+{
+    internal class Hotel
+    {
+        List<Pessoa> HospedesHotel = new();
+        List<Suite> SuitesHotel = new();
+
+        public void CadastrarHospedeNoHotel()
+        {
+            string nome = "", sobrenome = "";
+            Pessoa hospede;
+
+            Console.WriteLine("Digite o primeiro nome do hóspede a ser cadastrado (campo obrigatório): ");
+            (int top, int left) = Console.GetCursorPosition();
+            while (string.IsNullOrEmpty(nome))
+            {
+                Console.SetCursorPosition(top, left);
+                nome = Console.ReadLine();
+
+            };
+
+            Console.WriteLine("Digite o sobrenome do hóspede a ser cadastrado: ");
+            sobrenome = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(sobrenome))
+            {
+                hospede = new(nome);
+            }
+            else
+            {
+                hospede = new(nome, sobrenome);
+            }
+            HospedesHotel.Add(hospede);
+            Console.WriteLine($"Hóspede cadastrado com sucesso: {hospede.NomeCompleto}");
+        }
+
+        public void CadastrarSuitesNoHotel()
+        {
+            string tipo = "";
+            int capacidade;
+            decimal valor;
+            Suite suite;
+
+            Console.WriteLine("Digite o Tipo da suíte (obrigatório): ");
+            (int top, int left) = Console.GetCursorPosition();
+            while (string.IsNullOrEmpty(tipo))
+            {
+                Console.SetCursorPosition(top, left);
+                tipo = Console.ReadLine();
+            };
+
+            Console.WriteLine("Digite a capacidade da suíte (número inteiro positivo): ");
+            (top, left) = Console.GetCursorPosition();
+            while (!int.TryParse(Console.ReadLine(), out capacidade) || capacidade <= 0)
+            {
+                Console.SetCursorPosition(top, left);
+                Console.WriteLine(new string(' ', 300));
+                Console.SetCursorPosition(top, left);
+            }
+
+            Console.WriteLine("Digite o valor da diária da suíte (número decimal positivo): ");
+            (top, left) = Console.GetCursorPosition();
+            while (!decimal.TryParse(Console.ReadLine(), out valor) || valor <= 0)
+            {
+                Console.SetCursorPosition(top, left);
+                Console.WriteLine(new string(' ', 300));
+                Console.SetCursorPosition(top, left);
+            }
+
+            suite = new(tipo, capacidade, valor);
+            SuitesHotel.Add(suite);
+            Console.WriteLine($"Suíte cadastrada com sucesso: {suite.TipoSuite}");
+        }
+
+        public void CadastrarReserva()
+        {
+            Console.Clear();
+
+            int dias;
+            List<Pessoa> hospedesSelecionados = new();
+            (int top, int left) = (0, 0);
+
+            Console.WriteLine("--- Cadastro de Reserva ---");
+            Console.WriteLine("Digite a quantidade de dias que deseja reservar (número inteiro positivo): ");
+            (top, left) = Console.GetCursorPosition();
+            while (!int.TryParse(Console.ReadLine(), out dias) || dias <= 0)
+            {
+                Console.SetCursorPosition(top, left);
+                Console.WriteLine(new string(' ', 300));
+                Console.SetCursorPosition(top, left);
+            }
+            Reserva reserva = new(dias);
+
+            int reservaOpcao = 0;
+
+            while (reservaOpcao != 5)
+            {
+                Console.Clear();
+                Console.WriteLine("--- Cadastro de Reserva ---");
+                Console.WriteLine($"Reserva para {reserva.DiasReservados} dias. \n");
+
+                Console.WriteLine("1 - Inserir Hóspedes na reserva.");
+                Console.WriteLine("2 - Remover Hóspedes da reserva."); ;
+                Console.WriteLine("2 - Visualizar Dados da reserva.");
+                Console.WriteLine("4 - Escolher Suíte");
+                Console.WriteLine("5 - Confirmar Reserva");
+
+                Console.Write("Digite a opção: ");
+                int.TryParse(Console.ReadLine(), out reservaOpcao);
+
+                switch (reservaOpcao)
+                {
+                    case 1:
+                        {
+                            int hospedeId;
+
+                            Console.WriteLine("Lista de todos os hóspedes:");
+                            for (int i = 0; i < HospedesHotel.Count; i++)
+                            {
+                                Console.WriteLine($"Hóspede #{i + 1}: {HospedesHotel[i].NomeCompleto}");
+                            }
+
+                            Console.WriteLine("Digite o número do hóspede que deseja incluir na reserva (0 para cancelar): ");
+                            (top, left) = Console.GetCursorPosition();
+                            while (!int.TryParse(Console.ReadLine(), out hospedeId) || hospedeId < 0 || hospedeId > HospedesHotel.Count)
+                            {
+                                Console.SetCursorPosition(top, left);
+                                Console.WriteLine(new string(' ', 300));
+                                Console.SetCursorPosition(top, left);
+                            }
+
+                            if (hospedeId != 0)
+                            {
+                                try
+                                {
+                                    hospedesSelecionados.Add(HospedesHotel[hospedeId - 1]);
+                                    reserva.CadastrarHospedes(hospedesSelecionados);
+                                    Console.WriteLine("Hóspede incluído na reserva!");
+                                }
+                                catch (Exception ex)
+                                {
+                                    hospedesSelecionados.RemoveAt(hospedesSelecionados.Count - 1);
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine("Para cadastrar mais hóspedes, escolha uma nova suíte com capacidade maior.");
+                                }
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            int hospedeId;
+
+                            Console.WriteLine("Digite o número do hóspede que deseja remover da reserva (0 para cancelar): ");
+                            (top, left) = Console.GetCursorPosition();
+                            while (!int.TryParse(Console.ReadLine(), out hospedeId) || hospedeId < 0 || hospedeId > hospedesSelecionados.Count)
+                            {
+                                Console.SetCursorPosition(top, left);
+                                Console.WriteLine(new string(' ', 300));
+                                Console.SetCursorPosition(top, left);
+                            }
+
+                            if (hospedeId != 0)
+                            {
+                                hospedesSelecionados.RemoveAt(hospedeId - 1);
+                                Console.WriteLine("Hóspede removido da reserva.");
+                            }
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (hospedesSelecionados.Count == 0)
+                            {
+                                Console.WriteLine("Não há hóspedes cadastrados na reserva.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Lista de hóspedes na reserva:");
+                                for (int i = 0; i < hospedesSelecionados.Count; i++)
+                                {
+                                    Console.WriteLine($"Hóspede #{i + 1}: {hospedesSelecionados[i].NomeCompleto}");
+                                }
+                            }
+
+                            if (reserva.Suite == null)
+                            {
+                                Console.WriteLine("Não há suíte escolhida nessa reserva.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Suíte escolhida: {reserva.Suite.TipoSuite}, capacidade: {reserva.Suite.CapacidadeSuite}, valor da diária: {reserva.Suite.ValorDiaria:C}");
+                            }
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            int suiteId;
+
+                            Console.WriteLine("Lista de todas as suítes:");
+                            for (int i = 0; i < SuitesHotel.Count; i++)
+                            {
+                                Console.WriteLine($"Suíte #{i + 1}: {SuitesHotel[i].TipoSuite}, capacidade: {SuitesHotel[i].CapacidadeSuite}, valor da diária: {SuitesHotel[i].ValorDiaria:C}");
+                            }
+
+                            Console.WriteLine("Digite o número da suíte que deseja escolher para a reserva (0 para cancelar): ");
+                            (top, left) = Console.GetCursorPosition();
+                            while (!int.TryParse(Console.ReadLine(), out suiteId) || suiteId < 0 || suiteId > SuitesHotel.Count)
+                            {
+                                Console.SetCursorPosition(top, left);
+                                Console.WriteLine(new string(' ', 300));
+                                Console.SetCursorPosition(top, left);
+                            }
+
+                            if (suiteId != 0)
+                            {
+                                try
+                                {
+                                    reserva.CadastrarSuite(SuitesHotel[suiteId - 1]);
+                                    Console.WriteLine("Suíte incluída na reserva!");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine("Escolha uma nova suíte ou diminua a quantidade de hóspedes da reserva.");
+                                }
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (reserva.ObterQuantidadeHospedes() > 0 && reserva.Suite != null)
+                            {
+                                Console.WriteLine("Reserva realizada com sucesso!");
+                                Console.WriteLine($"Quantidade de hóspedes na reserva: {reserva.ObterQuantidadeHospedes()}");
+                                Console.WriteLine($"Valor total a ser pago: {reserva.CalcularValorDiaria():#.##}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("A reserva não pôde ser realizada pois há dados faltando.");
+                            }
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                        }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Projeto 03 - Sistema de Hospedagem/Hospedagem/Hospedagem/Program.cs
+++ Projeto 03 - Sistema de Hospedagem/Hospedagem/Hospedagem/Program.cs
@@ -1,7 +1,7 @@
-﻿using Hospedagem.Models;
+﻿using Hospedagem;
+using Hospedagem.Models;
 
-List<Pessoa> todosHospedes = new();
-List<Suite> todasSuites = new();
+Hotel hotel = new Hotel();
 
 int opcao = 0;
 
@@ -23,209 +23,37 @@ while (opcao != 5)
     {
         case 1:
             {
-                string nome = "", sobrenome = "";
-                Pessoa hospede;
-
-                Console.WriteLine("Digite o primeiro nome do hóspede a ser cadastrado (campo obrigatório): ");
-                while (string.IsNullOrEmpty(nome))
-                {
-                    nome = Console.ReadLine();
-                };
-
-                Console.WriteLine("Digite o sobrenome do hóspede a ser cadastrado: ");
-                sobrenome = Console.ReadLine();
-
-                if (string.IsNullOrEmpty(sobrenome))
-                {
-                    hospede = new(nome);
-                }
-                else
-                {
-                    hospede = new(nome, sobrenome);
-                }
-                todosHospedes.Add(hospede);
-                Console.WriteLine($"Hóspede cadastrado com sucesso: {hospede.NomeCompleto}");
+                hotel.CadastrarHospedeNoHotel();
                 break;
             }
         case 2:
             {
-                string tipo = "";
-                int capacidade;
-                decimal valor;
-                Suite suite;
-
-                Console.WriteLine("Digite o Tipo da suíte (obrigatório): ");
-                while (string.IsNullOrEmpty(tipo))
-                {
-                    tipo = Console.ReadLine();
-                };
-
-                Console.WriteLine("Digite a capacidade da suíte (obrigatório)(número inteiro positivo): ");
-                while (!int.TryParse(Console.ReadLine(), out capacidade) || capacidade <= 0) { }
-
-                Console.WriteLine("Digite o valor da diária da suíte (obrigatório)(número inteiro positivo): ");
-                while (!decimal.TryParse(Console.ReadLine(), out valor) || valor <= 0) { }
-
-                suite = new(tipo, capacidade, valor);
-                todasSuites.Add(suite);
-                Console.WriteLine($"Suíte cadastrada com sucesso: {suite.TipoSuite}");
+                hotel.CadastrarSuitesNoHotel();
                 break;
             }
         case 3:
             {
-                Console.Clear();
-
-                int dias;
-                List<Pessoa> hospedesSelecionados = new();
-
-                Console.WriteLine("--- Cadastro de Reserva ---");
-                Console.WriteLine("Digite a quantidade de dias que deseja reservar (número inteiro positivo): ");
-                while (!int.TryParse(Console.ReadLine(), out dias) || dias <= 0) { }
-                Reserva reserva = new(dias);
-
-                int reservaOpcao = 0;
-
-                while (reservaOpcao != 5)
-                {
-                    Console.Clear();
-                    Console.WriteLine("--- Cadastro de Reserva ---");
-                    Console.WriteLine($"Reserva para {reserva.DiasReservados} dias. \n");
-
-                    Console.WriteLine("1 - Inserir Hóspedes na reserva.");
-                    Console.WriteLine("2 - Remover Hóspedes da reserva."); ;
-                    Console.WriteLine("2 - Visualizar Dados da reserva.");
-                    Console.WriteLine("4 - Escolher Suíte");
-                    Console.WriteLine("5 - Finalizar");
-
-                    Console.Write("Digite a opção: ");
-                    int.TryParse(Console.ReadLine(), out reservaOpcao);
-
-                    switch (reservaOpcao)
-                    {
-                        case 1:
-                            {
-                                int hospedeId;
-
-                                Console.WriteLine("Lista de todos os hóspedes:");
-                                for (int i = 0; i < todosHospedes.Count; i++)
-                                {
-                                    Console.WriteLine($"Hóspede #{i + 1}: {todosHospedes[i].NomeCompleto}");
-                                }
-
-                                Console.WriteLine("Digite o número do hóspede que deseja incluir na reserva (0 para cancelar): ");
-                                while (!int.TryParse(Console.ReadLine(), out hospedeId) || hospedeId < 0 || hospedeId > todosHospedes.Count) { }
-
-                                if (hospedeId != 0)
-                                {
-                                    try
-                                    {
-                                        hospedesSelecionados.Add(todosHospedes[hospedeId - 1]);
-                                        reserva.CadastrarHospedes(hospedesSelecionados);
-                                        Console.WriteLine("Hóspede incluído na reserva!");
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        hospedesSelecionados.RemoveAt(hospedesSelecionados.Count - 1);
-                                        Console.WriteLine(ex.Message);
-                                        Console.WriteLine("Para cadastrar mais hóspedes, escolha uma nova suíte com capacidade maior.");
-                                    }
-                                }
-
-                                break;
-                            }
-                        case 2:
-                            {
-                                int hospedeId;
-
-                                Console.WriteLine("Digite o número do hóspede que deseja remover da reserva (o para cancelar): ");
-                                while (!int.TryParse(Console.ReadLine(), out hospedeId) || hospedeId < 0 || hospedeId > todosHospedes.Count) { }
-
-                                if (hospedeId != 0)
-                                {
-                                    hospedesSelecionados.RemoveAt(hospedeId - 1);
-                                }
-
-                                break;
-                            }
-                        case 3:
-                            {
-                                Console.WriteLine("Lista de hóspedes na reserva:");
-                                for (int i = 0; i < hospedesSelecionados.Count; i++)
-                                {
-                                    Console.WriteLine($"Hóspede #{i + 1}: {hospedesSelecionados[i].NomeCompleto}");
-                                }
-
-                                if (reserva.Suite == null)
-                                {
-                                    Console.WriteLine("Não há suíte escolhida nessa reserva.");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Suíte escolhida: {reserva.Suite.TipoSuite}, capacidade: {reserva.Suite.CapacidadeSuite}, valor da diária: {reserva.Suite.ValorDiaria:C}");
-                                }
-
-                                break;
-                            }
-                        case 4:
-                            {
-                                int suiteId;
-
-                                Console.WriteLine("Lista de todas as suítes:");
-                                for (int i = 0; i < todasSuites.Count; i++)
-                                {
-                                    Console.WriteLine($"Suíte #{i + 1}: {todasSuites[i].TipoSuite}, capacidade: {todasSuites[i].CapacidadeSuite}, valor da diária: {todasSuites[i].ValorDiaria:C}");
-                                }
-
-                                Console.WriteLine("Digite o número da suíte que deseja escolher para a reserva (0 para cancelar): ");
-                                while (!int.TryParse(Console.ReadLine(), out suiteId) || suiteId < 0 || suiteId > todasSuites.Count) { }
-
-                                if (suiteId != 0)
-                                {
-                                    try
-                                    {
-                                        reserva.CadastrarSuite(todasSuites[suiteId - 1]);
-                                        Console.WriteLine("Suíte incluída na reserva!");
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        Console.WriteLine(ex.Message);
-                                        Console.WriteLine("Escolha uma nova suíte ou diminua a quantidade de hóspedes da reserva.");
-                                    }
-                                }
-                                break;
-                            }
-                        case 5:
-                            {
-                                Console.WriteLine("Reserva realizada com sucesso!");
-                                Console.WriteLine($"Quantidade de hóspedes na reserva: {reserva.ObterQuantidadeHospedes()}");
-                                Console.WriteLine($"Valor total a ser pago: {reserva.CalcularValorDiaria():#.##}");
-                                break;
-                            }
-                        default:
-                            {
-                                Console.WriteLine("Opção inválida!");
-                                break;
-                            }
-                    }
-                    Console.ReadLine();
-                }
+                hotel.CadastrarReserva();
                 break;
             }
         case 4:
             {
-                Console.WriteLine("Pressione Enter para realizar o teste padrão.");
-                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("--- Teste padrão ---");
 
                 Pessoa hospede1 = new("João", "da Silva");
                 Pessoa hospede2 = new("José");
-
                 List<Pessoa> listaHospedes = new() { hospede1, hospede2 };
+                Suite suite = new("Basica", 2, 80.00M);
+                int dias = 10;
 
-                Suite suiteBasica = new("Basica", 2, 80.00M);
+                Console.WriteLine($"Hóspedes: \n{listaHospedes[0].NomeCompleto} \n{listaHospedes[1].NomeCompleto}");
+                Console.WriteLine($"Suite: {suite.TipoSuite}, capacidade: {suite.CapacidadeSuite}, valor da diária: {suite.ValorDiaria}");
+                Console.WriteLine($"Dias reservados: {dias} \n");
 
-                Reserva reserva = new(diasReservados: 10);
+                Reserva reserva = new(diasReservados: dias);
                 reserva.CadastrarHospedes(listaHospedes);
-                reserva.CadastrarSuite(suiteBasica);
+                reserva.CadastrarSuite(suite);
 
                 Console.WriteLine($"Quantidade de hóspedes na reserva: {reserva.ObterQuantidadeHospedes()}");
                 Console.WriteLine($"Valor total a ser pago: {reserva.CalcularValorDiaria():#.##}");
