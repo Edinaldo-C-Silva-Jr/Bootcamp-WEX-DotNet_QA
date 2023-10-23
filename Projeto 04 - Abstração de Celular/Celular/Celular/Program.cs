2d6e28156022b81f798142b1a61cc24c3f4a8c68
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-23 18:46:32 -0300

    Adiciona os testes dos celulares na classe Program.cs.

--- Projeto 04 - Abstração de Celular/Celular/Celular/Program.cs
+++ Projeto 04 - Abstração de Celular/Celular/Celular/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Celular.Models;
+
+Nokia celularNokia = new("12345678", "Nokia 1", "1234", 16);
+Iphone celularIphone = new("87654321", "Iphone 1", "4321", 16);
+
+Console.WriteLine($"Celular Nokia. Número: {celularNokia.Numero} \n");
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine($"\nCelular Iphone. Número: {celularIphone.Numero} \n");
+celularIphone.Ligar();
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Youtube");
