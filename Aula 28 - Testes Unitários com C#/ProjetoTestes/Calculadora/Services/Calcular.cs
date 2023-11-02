d3469ed91fe90caab0738b0140bef891204d89ec
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-11-02 15:42:39 -0300

    Implementa os métodos restantes da classe Calculadora e os cenários de teste para esses métodos.

--- Aula 28 - Testes Unitários com C#/ProjetoTestes/Calculadora/Services/Calcular.cs
+++ Aula 28 - Testes Unitários com C#/ProjetoTestes/Calculadora/Services/Calcular.cs
@@ -6,5 +6,22 @@
         {
             return numero1 + numero2;
         }
+        public int Subtrair(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+        public int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+        public int Dividir(int numero1, int numero2)
+        {
+            if (numero2 != 0)
+            { 
+                return numero1 / numero2;
+            }
+
+            return 0;
+        }
     }
 }
--- Aula 28 - Testes Unitários com C#/ProjetoTestes/CalculadoraTestes/CalcularTests.cs
+++ Aula 28 - Testes Unitários com C#/ProjetoTestes/CalculadoraTestes/CalcularTests.cs
@@ -23,5 +23,109 @@ namespace CalculadoraTestes
             // Assert
             Assert.Equal(15, resultado);
         }
+
+        [Fact]
+        public void DeveSomarMenos10Com20ERetornar10()
+        {
+            // Arrange
+            int n1 = -10, n2 = 20;
+
+            // Act
+            int resultado = _calcular.Somar(n1, n2);
+
+            // Assert
+            Assert.Equal(10, resultado);
+        }
+
+        [Fact]
+        public void DeveSubtrair8De40ERetornar32()
+        {
+            // Arrange
+            int n1 = 40, n2 = 8;
+
+            // Act
+            int resultado = _calcular.Subtrair(n1, n2);
+
+            // Assert
+            Assert.Equal(32, resultado);
+        }
+
+        [Fact]
+        public void DeveSubtrairMenos10De20ERetornar30()
+        {
+            // Arrange
+            int n1 = 20, n2 = -10;
+
+            // Act
+            int resultado = _calcular.Subtrair(n1, n2);
+
+            // Assert
+            Assert.Equal(30, resultado);
+        }
+
+        [Fact]
+        public void DeveMultiplicar5Por4ERetornar20()
+        {
+            // Arrange
+            int n1 = 5, n2 = 4;
+
+            // Act
+            int resultado = _calcular.Multiplicar(n1, n2);
+
+            // Assert
+            Assert.Equal(20, resultado);
+        }
+
+        [Fact]
+        public void DeveMultiplicarMenos6Por3ERetornarMenos18()
+        {
+            // Arrange
+            int n1 = -6, n2 = 3;
+
+            // Act
+            int resultado = _calcular.Multiplicar(n1, n2);
+
+            // Assert
+            Assert.Equal(-18, resultado);
+        }
+
+        [Fact]
+        public void DeveDividir50Por5ERetornar10()
+        {
+            // Arrange
+            int n1 = 50, n2 = 5;
+
+            // Act
+            int resultado = _calcular.Dividir(n1, n2);
+
+            // Assert
+            Assert.Equal(10, resultado);
+        }
+
+        [Fact]
+        public void DeveDividirMenos100Por20ERetornarMenos5()
+        {
+            // Arrange
+            int n1 = -100, n2 = 20;
+
+            // Act
+            int resultado = _calcular.Dividir(n1, n2);
+
+            // Assert
+            Assert.Equal(-5, resultado);
+        }
+
+        [Fact]
+        public void DeveRetornar0AoDividirPor0()
+        {
+            // Arrange
+            int n1 = 12345, n2 = 0;
+
+            // Act
+            int resultado = _calcular.Dividir(n1, n2);
+
+            // Assert
+            Assert.Equal(0, resultado);
+        }
     }
 }
