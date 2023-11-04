b68d152f53681225b5045f5f4e1c7f3e20a6272c
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-11-04 13:45:36 -0300

    Implementa os testes necessários para o Desafio de Projeto.

---
+++ Projeto 07 - Testes Unitários/TestesUnitarios/ValidacoesTestes/ValidacoesListaTests.cs
@@ -0,0 +1,98 @@
+using Validacoes.Services;
+
+namespace ValidacoesTestes
+{
+    public class ValidacoesListaTests
+    {
+        private readonly ValidacoesLista _validaLista;
+
+        public ValidacoesListaTests()
+        {
+            _validaLista = new ValidacoesLista();
+        }
+
+        [Fact]
+        public void DeveRemoverNumerosNegativosDeUmaLista()
+        {
+            // Arrange
+            List<int> lista = new() { 2, 5, -9, -1, 4, -3 };
+            List<int> listaEsperada = new() { 2, 5, 4 };
+
+            // Act
+            lista = _validaLista.RemoverNumerosNegativos(lista);
+
+            // Assert
+            Assert.Equal(listaEsperada, lista);
+        }
+
+        [Fact]
+        public void DeveConterONumero9NaLista()
+        {
+            // Arrange
+            List<int> lista = new() { 1, 3, 5, 7, 9 };
+            bool contemNumero;
+
+            // Act
+            contemNumero = _validaLista.ListaContemDeterminadoNumero(lista, 9);
+
+            // Assert
+            Assert.True(contemNumero);
+        }
+
+        [Fact]
+        public void NaoDeveConterONumero10NaLista()
+        {
+            // Arrange
+            List<int> lista = new() { 1, 3, 5, 7, 9 };
+            bool contemNumero;
+
+            // Act 
+            contemNumero = _validaLista.ListaContemDeterminadoNumero(lista, 10);
+
+            // Assert
+            Assert.False(contemNumero);
+        }
+
+        [Fact]
+        public void DeveMultiplicarOsElementosDaListaPor2()
+        {
+            // Arrange
+            List<int> lista = new() { 12, 8, 3, 95, 17 };
+            List<int> listaEsperada = new() { 24, 16, 6, 190, 34 };
+
+            // Act
+            lista = _validaLista.MultiplicarNumerosLista(lista, 2);
+
+            // Assert
+            Assert.Equal(listaEsperada, lista);
+        }
+
+        [Fact]
+        public void DeveRetornar9ComoMaiorNumeroDaLista()
+        {
+            // Arrange
+            List<int> lista = new() { -4, 8, 9, 0, -12, -648 };
+            int numero;
+
+            // Act
+            numero = _validaLista.RetornarMaiorNumeroLista(lista);
+
+            // Assert
+            Assert.Equal(9, numero);
+        }
+
+        [Fact]
+        public void DeveRetornarOitoNegativoComoMenorNumeroDaLista()
+        {
+            // Arrange
+            List<int> lista = new() { -8, 19, 1, 0, -5, 75 };
+            int numero;
+
+            // Act
+            numero = _validaLista.RetornarMenorNumeroLista(lista);
+
+            // Assert
+            Assert.Equal(-8, numero);
+        }
+    }
+}
---
+++ Projeto 07 - Testes Unitários/TestesUnitarios/ValidacoesTestes/ValidacoesStringTests.cs
@@ -0,0 +1,70 @@
+﻿using Validacoes.Services;
+
+namespace ValidacoesTestes
+{
+    public class ValidacoesStringTests
+    {
+        private readonly ValidacoesString _validaString;
+
+        public ValidacoesStringTests()
+        {
+            _validaString = new ValidacoesString();
+        }
+
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            // Arrange
+            int quantidadeCaracteres;
+            string stringDeTeste = "Matrix";
+
+            // Act
+            quantidadeCaracteres = _validaString.RetornarQuantidadeCaracteres(stringDeTeste);
+
+            // Assert
+            Assert.Equal(6, quantidadeCaracteres);
+        }
+
+        [Fact]
+        public void DeveContemAPalavraQualquerNoTexto()
+        {
+            // Arrange
+            bool palavraExiste;
+            string texto = "Este é um texto qualquer.", palavra = "qualquer";
+
+            // Act
+            palavraExiste = _validaString.ContemCaractere(texto, palavra);
+
+            // Assert
+            Assert.True(palavraExiste);
+        }
+
+        [Fact]
+        public void NaoDeveConterAPalavraTesteNoTexto()
+        {
+            // Arrange
+            bool palavraExiste;
+            string texto = "Este é um texto qualquer.", palavra = "teste";
+
+            // Act
+            palavraExiste = _validaString.ContemCaractere(texto, palavra);
+
+            // Assert
+            Assert.False(palavraExiste);
+        }
+
+        [Fact]
+        public void TextoDeveTerminarComAPalavraProcurado()
+        {
+            // Arrange
+            bool terminaComPalavra;
+            string texto = "Começo, meio e fim do texto procurado", palavra = "procurado";
+
+            // Act
+            terminaComPalavra = _validaString.TextoTerminaCom(texto, palavra);
+
+            // Assert
+            Assert.True(terminaComPalavra);
+        }
+    }
+}
